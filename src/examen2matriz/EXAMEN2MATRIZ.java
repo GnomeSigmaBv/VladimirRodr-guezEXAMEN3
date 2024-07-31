0c941372375cc632f925a6d017e654e6d6aec100
Vladimir Rodríguez <jacitoshadow@gmail.com>
2024-07-31 07:43:51 -0600

    Proyecto para el profe porque no pude hacer la matriz

---
+++ src/examen2matriz/EXAMEN2MATRIZ.java
@@ -0,0 +1,19 @@
+
+package examen2matriz;
+
+
+public class EXAMEN2MATRIZ {
+
+    public static void main(String[] args) {
+        
+        
+        int Datos [][] = {{10, 20, 30, 40},
+                        {1, 2, 3, 4},
+                        {20, 21, 22, 23}};
+        
+        for (int R = 0; R < Datos.length; R++) {
+            System.out.println(Datos);
+        }
+    }
+    
+}
